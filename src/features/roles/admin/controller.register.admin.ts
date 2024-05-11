20fc4da11ff0a001e477c29109e09ceb8f3b00c0
Zlaam <mrzalaam@gmail.com>
2024-05-11 23:08:17 +0500

    update controller.register.admin.ts

--- src/features/roles/admin/controller.register.admin.ts
+++ src/features/roles/admin/controller.register.admin.ts
@@ -1,15 +1,18 @@
 // controllers/admin/registerAdmin.ts
+//TODO: Sovle the following bug "[1] Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client"
 import type { NextFunction, Request, Response } from "express";
 import ApiResponse from "../../../utils/ApiResponse";
-import ApiErrors from "../../../utils/ApiError";
 import { AdminTypes } from "./types.admin";
 import { isAuthenticated } from "../../../utils/isAuthenticated";
 import { asyncHandler } from "../../../utils/asynchandler";
 
 export const registerAdmin = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const props = req.body;
-    isAuthenticated(props, res);
+    const props: AdminTypes = req.body;
+    let isAuthenticate = isAuthenticated(props, res);
+    if (!isAuthenticate) {
+      return res.send("Not allowed");
+    }
     return res.json(ApiResponse(200, "OK"));
   }
 );
