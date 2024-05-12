85342d957dac93a73a5386d46249fd8357cbabf9
Zlaam <mrzalaam@gmail.com>
2024-05-12 13:24:46 +0500

    update controller.register.admin.ts

--- src/features/roles/admin/controller.register.admin.ts
+++ src/features/roles/admin/controller.register.admin.ts
@@ -3,10 +3,12 @@ import ApiResponse from "../../../utils/ApiResponse";
 import { AdminTypes } from "./types.admin";
 import { isAuthenticated } from "../../../utils/isAuthenticated";
 import { asyncHandler } from "../../../utils/asynchandler";
+import { AdminModel } from "./model.admin";
 
 export const registerAdmin = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const props: AdminTypes = req.body;
+    const { username, email, fullName, password, role } = props;
 
     const authResult = isAuthenticated(props);
     if (typeof authResult !== "boolean" && authResult.statusCode >= 400) {
@@ -20,7 +22,22 @@ export const registerAdmin = asyncHandler(
           )
         );
     }
+    const isAdminRoleExist = await AdminModel.findOne({ role: "admin" });
+    if (isAdminRoleExist)
+      return res
+        .status(409)
+        .json(ApiResponse(409, "Only One admin is allowed at one time!"));
+    const isAdminExist = await AdminModel.findOne({
+      $or: [{ username }, { email }],
+    });
+    if (isAdminExist) {
+      return res
+        .status(409)
+        .json(
+          ApiResponse(409, "user is already exist with same name or email!")
+        );
+    }
 
-    return res.json(ApiResponse(200, "OK"));
+    return res.json(ApiResponse(200, "Admin registered successfully"));
   }
 );
