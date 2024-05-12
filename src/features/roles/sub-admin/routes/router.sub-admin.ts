bb65700b5d26296c4a19760f9b98b17671e5dd9d
Zlaam <mrzalaam@gmail.com>
2024-05-12 18:04:36 +0500

    update router.sub-admin.ts

--- src/features/roles/sub-admin/routes/router.sub-admin.ts
+++ src/features/roles/sub-admin/routes/router.sub-admin.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import getAdmin from "../controllers/controller.getSubAdmin";
+import getSubAdmin from "../controllers/controller.getSubAdmin";
 import registerSubAdmin from "../controllers/controller.register.sub-admin";
 import subAdminLogin from "../controllers/controller.login.sub-admin";
 
@@ -7,4 +7,4 @@ export const subAdminRouter = Router();
 
 subAdminRouter.post("/sub-admin/register", registerSubAdmin);
 subAdminRouter.post("/sub-admin/login", subAdminLogin);
-// subAdminRouter.get("/admin/all", getAdmin);
+subAdminRouter.get("/sub-admin/all", getSubAdmin);
