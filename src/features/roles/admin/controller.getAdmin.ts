407bd5fa9a21faba5049b4c6acf545b36a57eb8f
Zlaam <mrzalaam@gmail.com>
2024-05-12 14:53:14 +0500

    create 2 files and update 2 files

---
+++ src/features/roles/admin/controller.getAdmin.ts
@@ -0,0 +1,13 @@
+import type { NextFunction, Request, Response } from "express";
+import { asyncHandler } from "../../../utils/asynchandler";
+import { AdminModel } from "./model.admin";
+import ApiResponse from "../../../utils/ApiResponse";
+
+export default asyncHandler(async function GetAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const admin = await AdminModel.find().select("-password");
+  return res.status(200).json(ApiResponse(200, "OK", null, admin));
+});
--- src/features/roles/admin/router.admin.ts
+++ src/features/roles/admin/router.admin.ts
@@ -1,10 +1,9 @@
 import ApiResponse from "../../../utils/ApiResponse";
 import { Router } from "express";
-import { registerAdmin } from "./controller.register.admin";
+import registerAdmin from "./controller.register.admin";
+import getAdmin from "./controller.getAdmin";
 
 export const authRouter = Router();
 
-authRouter.get("/admin", (req, res) => {
-  return res.json(ApiResponse(res.statusCode, "AminRoute is Working"));
-});
 authRouter.post("/admin", registerAdmin);
+authRouter.get("/admin", getAdmin);
