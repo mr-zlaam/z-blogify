d81f0fe8de281fa783b438d7c706c4bb7dbf7e32
Zlaam <mrzalaam@gmail.com>
2024-05-13 18:55:09 +0500

    update updateBlogAuth.middleware.ts

--- src/middlewares/updateBlogAuth.middleware.ts
+++ src/middlewares/updateBlogAuth.middleware.ts
@@ -1,9 +1,10 @@
-import { type NextFunction, type Request, type Response } from "express";
-import { verify } from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
+import { verify, JsonWebTokenError } from "jsonwebtoken"; // Import JsonWebTokenError to catch token validation errors
 import { asyncHandler } from "../utils/asynchandler";
 import { _config } from "../config/config";
 import ApiResponse from "../utils/ApiResponse";
 import { UserModel } from "../features/roles/users/models/model.user";
+
 export interface AuthRequest extends Request {
   authorId: string;
 }
@@ -16,19 +17,31 @@ export default asyncHandler(async function updateBlogAuthMiddleware(
   const { JWT_ACCESS_SECRET } = _config;
   const token = req.header("Authorization");
   console.log("I am admin");
+
   if (!token)
-    return next(
-      res
-        .status(401)
-        .json(ApiResponse(401, "You are not authenticate to make changes"))
-    );
+    return res
+      .status(401)
+      .json(ApiResponse(401, "You are not authenticated to make changes")); // Changed the response message
+
   const parsedToken = token?.split(" ")[1] as string;
-  const decoded = verify(parsedToken, JWT_ACCESS_SECRET);
-  if (!decoded)
-    return next(res.status(401).json(ApiResponse(401, "Token is invalid")));
+  let decoded;
+
+  try {
+    decoded = verify(parsedToken, JWT_ACCESS_SECRET);
+  } catch (error) {
+    if (error instanceof JsonWebTokenError) {
+      // Handle token validation errors
+      return res.status(401).json(ApiResponse(401, "Token is invalid"));
+    } else {
+      return next(error); // Forward other errors to the error handling middleware
+    }
+  }
+
+  console.log(decoded);
   const _req = req as AuthRequest;
   _req.authorId = decoded.sub as string;
   const userId = _req.authorId;
+
   let isAdminOrSubAdmin;
   try {
     isAdminOrSubAdmin = await UserModel.findById(userId);
@@ -39,11 +52,12 @@ export default asyncHandler(async function updateBlogAuthMiddleware(
         .json(
           ApiResponse(
             500,
-            "Internal server error while checking isAdmin or not"
+            "Internal server error while checking if user is admin"
           )
         )
     );
   }
+
   const Admin = isAdminOrSubAdmin?.role === "admin";
 
   if (!Admin) {
