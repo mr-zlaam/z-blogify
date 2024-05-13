195efa570fe23b3da15b40c6a9ea1289341c8140
Zlaam <mrzalaam@gmail.com>
2024-05-13 16:46:32 +0500

    update tokenAuth.middleware.ts

--- src/middlewares/tokenAuth.middleware.ts
+++ src/middlewares/tokenAuth.middleware.ts
@@ -3,6 +3,7 @@ import { verify } from "jsonwebtoken";
 import { asyncHandler } from "../utils/asynchandler";
 import { _config } from "../config/config";
 import ApiResponse from "../utils/ApiResponse";
+import { UserModel } from "../features/roles/users/models/model.user";
 export interface AuthRequest extends Request {
   authorId: string;
 }
@@ -26,5 +27,34 @@ export default asyncHandler(async function authTokenAuthenticator(
     return next(res.status(401).json(ApiResponse(401, "Token is invalid")));
   const _req = req as AuthRequest;
   _req.authorId = decoded.sub as string;
+  const userId = _req.authorId;
+  let isAdminOrSubAdmin;
+  try {
+    isAdminOrSubAdmin = await UserModel.findById(userId);
+  } catch (error: any) {
+    return next(
+      res
+        .status(500)
+        .json(
+          ApiResponse(
+            500,
+            "Internal server error while checking isAdmin or not"
+          )
+        )
+    );
+  }
+  const Admin = isAdminOrSubAdmin?.role === "admin";
+  const SubAdmin = isAdminOrSubAdmin?.role === "sub-admin";
+  if (!Admin && !SubAdmin) {
+    return res
+      .status(403)
+      .json(
+        ApiResponse(
+          403,
+          "You are forbidden to make changes as you are not admin or sub-admin"
+        )
+      );
+  }
+
   return next();
 });
