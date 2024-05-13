54bbd5a904e1155280c245ad1c3ee14209114ba5
Zlaam <mrzalaam@gmail.com>
2024-05-13 20:36:33 +0500

    feat: create controller.deleteUser.ts

---
+++ src/features/roles/users/controllers/controller.deleteUser.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { asyncHandler } from "../../../../utils/asynchandler";
+import { UserModel } from "../models/model.user";
+import ApiResponse from "../../../../utils/ApiResponse";
+
+export default asyncHandler(async function deleteUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { userId } = req.params;
+  let deleteThisUser;
+  try {
+    deleteThisUser = await UserModel.findOneAndDelete({ _id: userId });
+  } catch (error: any) {
+    console.log(error.message);
+    return next(
+      res
+        .status(500)
+        .json(
+          ApiResponse(500, "internal server error while deleting this user")
+        )
+    );
+  }
+  return res
+    .status(201)
+    .json(ApiResponse(201, "Deleted this user successfully."));
+});
