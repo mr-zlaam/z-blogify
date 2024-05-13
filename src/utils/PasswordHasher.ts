37e5b747e92650ee03d554f27c2942dd174c12e3
Zlaam <mrzalaam@gmail.com>
2024-05-13 21:28:43 +0500

    delete 1 file and update 1 file

--- src/utils/PasswordHasher.ts
+++ src/utils/PasswordHasher.ts
@@ -1,11 +1,17 @@
 import bcrypt from "bcrypt";
+import { Response } from "express";
+import ApiResponse from "./ApiResponse";
 
-export const passwordHasher = async (password: string) => {
+export const passwordHasher = async (password: string, res: Response) => {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     return hashedPassword;
   } catch (error: any) {
     console.log(error.message);
-    return;
+    return res
+      .status(500)
+      .json(
+        ApiResponse(500, "internal server error while hashing the password")
+      );
   }
 };
