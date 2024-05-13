2b477edc4eeb8969671828e2cfd6d1127afb1dde
Zlaam <mrzalaam@gmail.com>
2024-05-13 17:27:06 +0500

    update controller.draftBlog.ts and router.blog.ts

--- src/features/blogs/controllers/controller.draftBlog.ts
+++ src/features/blogs/controllers/controller.draftBlog.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import { asyncHandler } from "../../../utils/asynchandler";
 import { BlogModel } from "../models/model.bog";
+import ApiResponse from "../../../utils/ApiResponse";
 
 export default asyncHandler(async function draftBlogs(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const drafBLogsList = await BlogModel.find();
+  const draftBlogList = await BlogModel.find();
+  if (draftBlogList.length === 0) {
+    return next(res.status(404).json(ApiResponse(400, "No draft Blogs Found")));
+  }
+  return res.status(200).json(ApiResponse(200, "OK", null, draftBlogList));
 });
--- src/features/blogs/routes/router.blog.ts
+++ src/features/blogs/routes/router.blog.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import authTokenAuthenticator from "../../../middlewares/tokenAuth.middleware";
 import createBlog from "../controllers/controller.createBlog";
+import draftBlogs from "../controllers/controller.draftBlog";
 export const blogRouter = Router();
 
 blogRouter.post("/createBlog", authTokenAuthenticator, createBlog);
+blogRouter.get("/draftBlogs", draftBlogs);
