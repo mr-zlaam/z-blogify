6e25af8a3b8cdd43d97081520b48eaa13e503428
Zlaam <mrzalaam@gmail.com>
2024-05-14 22:25:42 +0500

    frontend/src/validation/Schemas/registerSchema: correct the email regex

--- frontend/src/validation/Schemas/registerSchema.ts
+++ frontend/src/validation/Schemas/registerSchema.ts
@@ -18,10 +18,8 @@ export const registerSchema: ZodType<UserRegisterTypes> = object({
   email: string()
     .email()
     .min(5, { message: "This field requires at least 5 characters" })
-    .max(20, { message: "Email is too long" })
-    .regex(new RegExp(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/), {
-      message: "Please provide valid email",
-    })
+    .max(30, { message: "Email is too long" })
+
     .toLowerCase(),
   password: string()
     .min(6, { message: "This field requires at least 6 characters" })
