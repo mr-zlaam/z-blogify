e9a492cb51bb2fa55c58a12b441c41748213b300
Zlaam <mrzalaam@gmail.com>
2024-05-14 21:34:47 +0500

    backend: add cookie-parser and cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,11 @@ import express, { Express } from "express";
 import cors from "cors";
 import { userRouter } from "./features/roles/users/routes/router.user";
 import { blogRouter } from "./features/blogs/routes/router.blog";
+import cookieParser from "cookie-parser";
 
 export const app: Express = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 //Authentication
