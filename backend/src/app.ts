0d110b14d07210d6e88a5990ec589e73124bf2bf
Zlaam <mrzalaam@gmail.com>
2024-05-14 21:28:43 +0500

    backend/src/app: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
 import express, { Express } from "express";
-
+import cors from "cors";
 import { userRouter } from "./features/roles/users/routes/router.user";
 import { blogRouter } from "./features/blogs/routes/router.blog";
 
 export const app: Express = express();
-
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 //Authentication
