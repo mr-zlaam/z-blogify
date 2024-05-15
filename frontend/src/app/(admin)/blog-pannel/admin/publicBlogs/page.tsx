53b1fafec3d3924d09fdc1751a53ade5fcc55068
Zlaam <mrzalaam@gmail.com>
2024-05-15 23:32:51 +0500

    🐳 chore:create private and public blog page

---
+++ frontend/src/app/(admin)/blog-pannel/admin/publicBlogs/page.tsx
@@ -0,0 +1,11 @@
+import {} from "react";
+
+function PublicBlogs() {
+  return (
+    <>
+      <section>PublicBlogs</section>
+    </>
+  );
+}
+
+export default PublicBlogs;
--- frontend/src/app/(admin)/blog-pannel/layout.tsx
+++ frontend/src/app/(admin)/blog-pannel/layout.tsx
@@ -53,7 +53,7 @@ export default function AdminLayout({
       <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
         <nav className="flex flex-col items-center gap-4 px-2 py-4">
           <Link
-            href="#"
+            href="/"
             className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
           >
             <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
@@ -63,7 +63,7 @@ export default function AdminLayout({
             <Tooltip>
               <TooltipTrigger asChild>
                 <Link
-                  href="#"
+                  href="/blog-pannel/admin/users"
                   className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                 >
                   <Users2 className="h-5 w-5" />
@@ -77,7 +77,7 @@ export default function AdminLayout({
             <Tooltip>
               <TooltipTrigger asChild>
                 <Link
-                  href="#"
+                  href="/blog-pannel/admin/privateBlogs"
                   className="flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8"
                 >
                   <BookLock className="h-5 w-5" />
@@ -91,7 +91,7 @@ export default function AdminLayout({
             <Tooltip>
               <TooltipTrigger asChild>
                 <Link
-                  href="#"
+                  href="/blog-pannel/admin/publicBlogs"
                   className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                 >
                   <BookOpenCheck className="h-5 w-5" />
@@ -115,28 +115,28 @@ export default function AdminLayout({
             <SheetContent side="left" className="sm:max-w-xs">
               <nav className="grid gap-6 text-lg font-medium">
                 <Link
-                  href="#"
+                  href="/"
                   className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
                 >
                   <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
                   <span className="sr-only">Zlaam</span>
                 </Link>
                 <Link
-                  href="#"
+                  href="/blog-pannel/admin/users"
                   className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                 >
                   <Users2 className="h-5 w-5" />
                   Users
                 </Link>
                 <Link
-                  href="#"
+                  href="/blog-pannel/admin/privateBlogs"
                   className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                 >
                   <BookLock className="h-5 w-5" />
                   Private Blogs
                 </Link>
                 <Link
-                  href="#"
+                  href="/blog-pannel/admin/publicBlogs"
                   className="flex items-center gap-4 px-2.5 text-foreground"
                 >
                   <BookOpenCheck className="h-5 w-5" />
