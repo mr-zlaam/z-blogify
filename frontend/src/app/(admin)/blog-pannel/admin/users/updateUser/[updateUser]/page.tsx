876c03356ffc8cee0c50a3a09444bf9d77c5d8eb
Zlaam <mrzalaam@gmail.com>
2024-05-17 00:59:47 +0500

    🐳 chore(fetch the single user data):

--- frontend/src/app/(admin)/blog-pannel/admin/users/updateUser/[updateUser]/page.tsx
+++ frontend/src/app/(admin)/blog-pannel/admin/users/updateUser/[updateUser]/page.tsx
@@ -1,9 +1,37 @@
+import axios from "axios";
 import {} from "react";
-
-function UpdateUser() {
+//@types
+interface ParamType {
+  updateUser: string;
+}
+const fetchSingleUser = async (updateUser: string) => {
+  console.log("User Id:", updateUser);
+  try {
+    const response = await axios.get(
+      `http://localhost:9000/api/v1/auth/user/getSingleUser/${updateUser}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjQyMzRmNTllOWZlNjY0MGFiZWE3MzEiLCJpYXQiOjE3MTU3MDUzMzcsImV4cCI6MTcxNjMxMDEzN30.JjKzzmJA6xfUbddHMAqx1b5okDWXUuTHvBM3s46SYVI",
+        },
+      }
+    );
+    console.log(response);
+    return response.data;
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+async function UpdateUser({ params }: { params: ParamType }) {
+  const { updateUser } = params;
+  const user = await fetchSingleUser(updateUser);
+  console.log(user);
   return (
     <>
-      <section>UpdateUser</section>
+      <section>
+        UpdateUser:
+        {updateUser}
+      </section>
     </>
   );
 }
