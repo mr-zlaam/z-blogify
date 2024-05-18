84f20ebfb8c92061f6ab5110cc9a7ea7a3767297
Zlaam <mrzalaam@gmail.com>
2024-05-18 19:55:06 +0500

    copy 1 file and update 1 file

--- frontend/src/app/(admin)/blog-pannel/admin/users/deleteUser/[deleteUser]/page.tsx
+++ frontend/src/app/(admin)/blog-pannel/admin/users/deleteUser/[deleteUser]/page.tsx
@@ -1,55 +1,16 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { useMessage } from "@/hooks/useMessage";
-import axios from "axios";
-import { useRouter } from "next/navigation";
 import {} from "react";
+import Delete from "./components/deleteModal";
+import useCookieGrabber from "@/hooks/useCookieGrabber";
 interface ParamType {
   deleteUser: string;
 }
-function Delete({ params }: { params: ParamType }) {
-  const router = useRouter();
-  const { deleteUser } = params;
-  const { errorMessage, successMessage } = useMessage();
-  const RedirectToPreviousPage = () => {
-    router.push("/blog-pannel/admin/users");
-  };
-  const deleteThisUser = async () => {
-    try {
-      const response = await axios.delete(
-        `http://localhost:9000/api/v1/auth/user/deleteUser/${deleteUser}`,
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjQyMzRmNTllOWZlNjY0MGFiZWE3MzEiLCJpYXQiOjE3MTU3MDUzMzcsImV4cCI6MTcxNjMxMDEzN30.JjKzzmJA6xfUbddHMAqx1b5okDWXUuTHvBM3s46SYVI",
-          },
-        }
-      );
-      if (response.status === 204) {
-        RedirectToPreviousPage();
-        return successMessage("user has been deleted successfully");
-      } else return null;
-    } catch (error: any) {
-      return errorMessage(
-        error.response.data.message ||
-          "something went wrong while deleting user."
-      );
-    }
-  };
+function DeleteUser({ params }: { params: ParamType }) {
+  const token = useCookieGrabber();
   return (
     <>
-      <section className="backdrop-blur-lg h-screen  absolute left-0 top-0 w-full z-50 px-5">
-        <div className="h-40 flex flex-col justify-around border bg-background border-foreground max-w-lg mx-auto relative top-80  rounded-md p-4 ">
-          <h1>Are You sure that you want to delete this user?</h1>
-          <p>{deleteUser}</p>
-          <div className="w-full flex justify-end items-center gap-5">
-            <Button onClick={RedirectToPreviousPage}>Close</Button>
-            <Button onClick={deleteThisUser}>Yes I am sure</Button>
-          </div>
-        </div>
-      </section>
+      <Delete id={params.deleteUser} token={token?.value || ""} />
     </>
   );
 }
 
-export default Delete;
+export default DeleteUser;
