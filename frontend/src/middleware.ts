12897e1888d22a2a5616023f25818bab5a0ee295
Zlaam <mrzalaam@gmail.com>
2024-05-19 15:11:27 +0500

    ✨ feat(protect the admin pannel):

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,8 +1,26 @@
-import { type NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { verify, JsonWebTokenError } from "jsonwebtoken";
 
 export default function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
+  const cookieStore = cookies();
+  const token = cookieStore.get("accessToken");
+  const tokenValue = token?.value as string;
+  if (request.nextUrl.pathname === "/admin/users") {
+    if (!token?.value) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+    const secret = process.env.JWT_ACCESS_SECRET as string;
+    let isTokenVerified;
+    try {
+      isTokenVerified = verify(tokenValue, secret);
+    } catch (error: any) {
+      if (error instanceof JsonWebTokenError) {
+        return NextResponse.redirect(new URL("/home", request.url));
+      }
+    }
+    // console.log(isTokenVerified);
+  }
+  if (request.nextUrl.pathname === "/")
+    return NextResponse.redirect(new URL("/home", request.url));
 }
-export const config = {
-  matcher: ["/"],
-};
