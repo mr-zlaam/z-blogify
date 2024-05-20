ef0d75127da56924dc6169c19b021f4ed8d527d8
Zlaam <mrzalaam@gmail.com>
2024-05-21 00:41:13 +0500

    update page.tsx

--- frontend/src/app/(pages)/home/page.tsx
+++ frontend/src/app/(pages)/home/page.tsx
@@ -1,6 +1,18 @@
+import { BACKEND_URI } from "@/config";
+import axios from "axios";
 import {} from "react";
-
-function HomePage() {
+const loadContent = async () => {
+  console.log("backend_uri", BACKEND_URI);
+  try {
+    const response = await axios.get(`${BACKEND_URI}/blogs/draftBlogs`);
+    return response.data;
+  } catch (error: any) {
+    console.log("error:", error.message);
+  }
+};
+async function HomePage() {
+  const data = await loadContent();
+  console.log(data);
   return (
     <>
       <section>HomePage</section>
