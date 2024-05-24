204149cd0ee1c61f84c596db93061b32ea2f2efb
Zlaam <mrzalaam@gmail.com>
2024-05-24 20:46:06 +0500

    update page.tsx

--- frontend/src/app/createBlog/page.tsx
+++ frontend/src/app/createBlog/page.tsx
@@ -1,9 +1,18 @@
 import {} from "react";
 import CreatePosts from "./components/Post";
+import Link from "next/link";
 
 function CreateBLog() {
   return (
     <>
+      <h1 className="py-5 bg-background z-[99]  text-center font-bold text-xl sticky top-0 w-full ">
+        <Link
+          href="/home"
+          className="duration-300 transition-all hover:text-blue-300"
+        >
+          Go To Home Page
+        </Link>
+      </h1>
       <CreatePosts />
     </>
   );
