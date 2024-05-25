fd9d3bb76fb98c9a1d480e65be8d9a5a9f4668b1
Zlaam <mrzalaam@gmail.com>
2024-05-25 17:20:24 +0500

    on the middlewares

--- backend/src/features/blogs/routes/router.blog.ts
+++ backend/src/features/blogs/routes/router.blog.ts
@@ -1,29 +1,25 @@
 import { Router } from "express";
-// import authTokenAuthenticator from "../../../middlewares/tokenAuth.middleware";
+import authTokenAuthenticator from "../../../middlewares/tokenAuth.middleware";
 import createBlog from "../controllers/controller.createBlog";
 import draftBlogs from "../controllers/controller.draftBlog";
 import publicBlogs from "../controllers/controller.publicBlogs";
 import updateBlog from "../controllers/controller.updateBlogs";
-// import updateAndDeleteAuthMiddleware from "../../../middlewares/dashboardAuth.middleware";
+import updateAndDeleteAuthMiddleware from "../../../middlewares/dashboardAuth.middleware";
 import deleteBlog from "../controllers/controller.deleteBlog";
 import getSingleBlog from "../controllers/controller.getSingleBlog";
 export const blogRouter = Router();
 
-blogRouter.post(
-  "/createBlog",
-  //  authTokenAuthenticator,
-  createBlog
-);
+blogRouter.post("/createBlog", authTokenAuthenticator, createBlog);
 blogRouter.get("/draftBlogs", draftBlogs);
 blogRouter.get("/publicBlogs", publicBlogs);
 blogRouter.get("/getSingleBlog/:blogSlug", getSingleBlog);
 blogRouter.patch(
   "/updateBlog/:slug",
-  // updateAndDeleteAuthMiddleware,
+  updateAndDeleteAuthMiddleware,
   updateBlog
 );
 blogRouter.delete(
   "/deleteBlog/:blogId",
-  // updateAndDeleteAuthMiddleware,
+  updateAndDeleteAuthMiddleware,
   deleteBlog
 );
--- frontend/src/app/(admin)/admin/blogs/publicBlogs/page.tsx
+++ frontend/src/app/(admin)/admin/blogs/publicBlogs/page.tsx
@@ -34,7 +34,8 @@ const fetchPublicBlogs = async () => {
     return error.response.data.statusCode || 403;
   }
 };
-export default function PublicBlogs() {
+export default async function PublicBlogs() {
+  const publicBlogs = await fetchPublicBlogs();
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
       <Tabs defaultValue="all">
