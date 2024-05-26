aab93ee35a7a7989f88550a02968334304aec09a
Zlaam <mrzalaam@gmail.com>
2024-05-27 01:10:10 +0500

    create 1 file and update 1 file

--- frontend/src/app/all-posts/page.tsx
+++ frontend/src/app/all-posts/page.tsx
@@ -1,11 +1,13 @@
 import {} from "react";
 import PageWrapper from "../components/PageWrapper/PageWrapper";
 import NavBar from "../components/NavBar/NavBar";
+import GoBack from "./components/GoBack";
 
 function AllPosts() {
   return (
     <>
       <PageWrapper>
+        <GoBack />
         <NavBar />
         <hr className="md:max-w-screen-xl mx-auto my-3 bg-foreground rounded h-1" />
       </PageWrapper>
