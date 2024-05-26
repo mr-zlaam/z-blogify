065d27d62eea2199fbbd30f501f957d6e54d75bd
Zlaam <mrzalaam@gmail.com>
2024-05-26 22:28:11 +0500

    update page.tsx, layout.tsx and globals.css

--- frontend/src/app/(pages)/layout.tsx
+++ frontend/src/app/(pages)/layout.tsx
@@ -6,16 +6,16 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <main className="flex   flex-col  min-h-screen">
-        <div className="fixed left-3 lg:top-9 top-[43px] md:top-5 z-[999]">
+        <div className="md:fixed md:left-3 lg:top-9 top-[43px] md:top-5 z-[999] m-4">
           <ThemeToggler />
         </div>
         <header className="z-[998] sticky top-0 ">{/* <Header /> */}</header>
         <section className="flex-[1] flex-grow relative z-[997]">
           {children}
         </section>
-        <footer className="absolute md:fixed bottom-2 right-5 text-xs z-[996]">
+        {/* <footer className="relative w-full mx-10 my-10 md:fixed bottom-2 right-5 text-xs z-[996]">
           &copy; 2024 &nbsp;&nbsp;&nbsp;&nbsp;&trade;Zlaam
-        </footer>
+        </footer> */}
       </main>
     </>
   );
