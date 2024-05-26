6c642a4ddf457859b14d22d717d97d2d3f63e427
Zlaam <mrzalaam@gmail.com>
2024-05-27 02:06:45 +0500

    update logo.tsx

--- frontend/src/app/(pages)/home/components/logo.tsx
+++ frontend/src/app/(pages)/home/components/logo.tsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <div className=" w-fit bg-foreground/70 flex justify-center items-center overflow-hidden rounded-full mx-auto my-5 shadow">
       <Image
-        src={"/logo/logo.jpeg"}
+        src={"/logo/Zlaam.jpg"}
         height={300}
         width={300}
         alt="Logo"
