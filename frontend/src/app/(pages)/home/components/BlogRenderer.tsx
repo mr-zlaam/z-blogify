171c7952ae6191c020150e202936cde4c54bd2ef
Zlaam <mrzalaam@gmail.com>
2024-05-26 20:15:04 +0500

    update BlogRenderer.tsx and logo.tsx

--- frontend/src/app/(pages)/home/components/BlogRenderer.tsx
+++ frontend/src/app/(pages)/home/components/BlogRenderer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Card } from "@/components/ui/card";
 import { PublicBLogTypes } from "@/types";
+import Image from "next/image";
 import { Fragment } from "react";
 
 export default function BlogRenderer({ data }: { data: PublicBLogTypes }) {
@@ -8,11 +9,26 @@ export default function BlogRenderer({ data }: { data: PublicBLogTypes }) {
   if (!data.success) return;
   return (
     <Fragment>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-3 gap-4">
         {latestPosts.publicBlogsList.map((post) => {
           return (
             <Fragment key={post._id}>
-              <Card className="min-h-[40dvh]">hello</Card>
+              <Card className="min-h-[50dvh] ronuded overflow-hidden">
+                <div className=" p-4 min-h-[60%] overflow-hidden">
+                  <Image
+                    src={post.blogThumbnail || "/logo/logo.jpeg"}
+                    alt={post.blogThumbnailAuthor || "zlaam"}
+                    className="rounded"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                </div>
+                <h1 className="text-xl font-bold text-balance px-4">
+                  {post.blogTitle}
+                </h1>
+              </Card>
             </Fragment>
           );
         })}
--- frontend/src/app/(pages)/home/components/logo.tsx
+++ frontend/src/app/(pages)/home/components/logo.tsx
@@ -3,7 +3,7 @@ import {} from "react";
 
 function Logo() {
   return (
-    <div className="h-[300px] w-[300px] bg-foreground/70 overflow-hidden rounded-full mx-auto my-5 shadow">
+    <div className="h-[300px] w-[300px] bg-foreground/70 flex justify-center items-center overflow-hidden rounded-full mx-auto my-5 shadow">
       <Image
         src={"/logo/logo.jpeg"}
         height={500}
