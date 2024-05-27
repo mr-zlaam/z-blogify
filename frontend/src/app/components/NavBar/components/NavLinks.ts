a267bab142d295f5739c45701ce8841132d23a6b
Zlaam <mrzalaam@gmail.com>
2024-05-27 15:33:57 +0500

    update NavLinks.ts

--- frontend/src/app/components/NavBar/components/NavLinks.ts
+++ frontend/src/app/components/NavBar/components/NavLinks.ts
@@ -10,14 +10,14 @@ export const navLinks = [
     name: "Latest Posts",
     path: "/home",
     className:
-      "border border-foreground transition-all duration-300 hover:bg-foreground hover:text-background",
+      "border   border-foreground transition-all duration-300 hover:bg-foreground hover:text-background",
     variant: "outline" as ButtonVariant,
   },
   {
     name: "All Posts",
     path: "/all-posts",
     className:
-      "border border-foreground transition-all duration-300 hover:bg-foreground hover:text-background",
+      "border  border-foreground transition-all duration-300 hover:bg-foreground hover:text-background",
     variant: "outline" as ButtonVariant,
   },
 ];
