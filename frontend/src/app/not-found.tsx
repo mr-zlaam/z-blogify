766b4f532811201048931d22ed7d8f60b020a2c5
Zlaam <mrzalaam@gmail.com>
2024-05-28 21:02:39 +0500

    feat: create not-found.tsx

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import {} from "react";
+
+function NotFound() {
+  return (
+    <>
+      <section className="h-[80dvh] flex flex-col justify-center items-center">
+        <h1 className="text-5xl font-bold my-10">404 | Not Found</h1>
+        <p>
+          <Link
+            href="/home"
+            className="text-3xl font-semibold  my-10 text-blue-400 hover:underline"
+          >
+            Go Home
+          </Link>
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default NotFound;
