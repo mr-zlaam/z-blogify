a26a09f860114a31f94d62742f74d32658ba8cdb
Zlaam <mrzalaam@gmail.com>
2024-05-30 17:12:27 +0500

    update page.tsx and globals.css

--- frontend/src/app/all-posts/page.tsx
+++ frontend/src/app/all-posts/page.tsx
@@ -4,9 +4,10 @@ import NavBar from "../components/NavBar/NavBar";
 import GoBack from "./components/GoBack/GoBack";
 import RenderAllPosts from "./components/RenderAllPosts/RenderAllPosts";
 import { fetchBlogs as FetchAllPost } from "../(pages)/home/page";
-import ImgLoader from '@/_subComponents/imgLoader'
+import ImgLoader from "@/_subComponents/imgLoader";
 import { PublicBLogTypes } from "@/types";
 import { useMessage } from "@/hooks/useMessage";
+import ScrollToTop from "../(getSinglePost)/1/[slug]/components/ScrollToTop";
 async function AllPosts() {
   const { errorMessage } = useMessage();
   const getAllPosts: PublicBLogTypes = await FetchAllPost();
@@ -16,13 +17,14 @@ async function AllPosts() {
   const allPosts = publicBlogsList.reverse();
   const renderLoader = () => (
     <div className="h-[50dvh] justify-center flex items-center">
-      <ImgLoader/>
+      <ImgLoader />
     </div>
   );
   return (
     <>
       <PageWrapper className="md:max-w-[920px]">
         <GoBack />
+        <ScrollToTop />
         <NavBar />
         <hr className="md:max-w-screen-xl mx-auto my-3 bg-foreground rounded h-1" />
         <Suspense fallback={renderLoader()}>
